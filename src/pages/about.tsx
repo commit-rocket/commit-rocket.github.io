4af5bfd233ab00f0b7d6543b90943f18f3bf3ca2
RikThePixel <Rikdenbreejen@hotmail.nl>
2023-03-12 00:58:31 +0100

    Make about more responsive

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,37 +5,39 @@ import LinkButton from "@/components/controls/LinkButton";
 import members, { IMember } from "@/assets/state/team";
 import { Page } from "@/types/page";
 
-const Member = ({ image, name, title, text, links }: IMember) => (
-  <div
-    className="flex flex-col items-center gap-2 p-6 rounded-md shadow w-fit shadow-primary"
+const Member = ({ image, name, title, text, links, animate }: IMember & { animate: boolean; }) => (
+  <motion.div
+    variants={animate ? memberAnim : {}}
+    className="flex flex-col items-center w-full max-w-full gap-2 p-4 rounded-md shadow motion-safe:transition-all sm:p-6 md:w-fit shadow-primary"
     aria-label="Member"
   >
-    <img
-      className="rounded-full shadow-md aspect-auto w-60 h-60"
-      aria-label="Link"
-      src={image.src}
-      width={image.width}
-      height={image.height}
-    />
-    <div className="flex flex-col gap-2 text-center w-72">
-      <p className="text-2xl font-semibold text-secondary" aria-label="Name">{name}</p>
-      <p className="font-semibold text-primary" aria-label="Title / Role">{title}</p>
-      <p className="text-fill-contrast" aria-label="Text">{text}</p>
+    <div className="flex flex-col items-center max-w-full gap-2 sm:flex-row md:flex-col">
+      <img
+        className="object-cover w-40 h-40 rounded-full shadow-md aspect-auto md:w-60 md:h-60"
+        aria-label="Link"
+        src={image.src}
+        width={image.width}
+        height={image.height}
+      />
+      <div className="flex flex-col max-w-full gap-2 py-4 text-center w-72 md:py-0">
+        <p className="text-2xl font-semibold text-secondary" aria-label="Name">{name}</p>
+        <p className="font-semibold text-primary" aria-label="Title / Role">{title}</p>
+        <p className="text-fill-contrast " aria-label="Text">{text}</p>
+      </div>
     </div>
-    <div className="grid w-full grid-cols-2 gap-2" aria-label="Links">
+    <div className="grid w-full grid-cols-1 gap-2 sm:grid-cols-2" aria-label="Links">
       {links.map((link, i) => (
         <LinkButton
           key={i}
           href={link.href}
           color="secondary"
           aria-label="Link"
-          className="last-of-type:odd:col-span-2"
         >
           {link.name}
         </LinkButton>
       ))}
     </div>
-  </div>
+  </motion.div>
 );
 
 const membersContainerAnim = {
@@ -65,15 +67,15 @@ const AboutPage: Page = ({ initialLoad, reduceMotion }) => {
         <title>About - Commit Rocket</title>
       </Head>
       <main aria-labelledby="about" className="flex flex-col flex-1 w-full gap-4 pb-8 max-w-7xl">
-        <h1 id="about" className="text-6xl font-bold text-center text-secondary">About</h1>
+        <h1 id="about" className="text-4xl font-bold text-center lg:text-6xl text-secondary">About</h1>
         <section aria-labelledby="our-goal" className="flex flex-col gap-4">
           <h2
             id="our-goal"
-            className="text-3xl text-center text-primary"
+            className="text-2xl text-center lg:text-3xl text-primary"
           >
             Our Goal
           </h2>
-          <p className="text-lg">
+          <p className="lg:text-lg">
             Our main goal with Commit Rocket is to drive innovation and provide a modern and fast alternative to existing Git clients.
             We want to improve the user experience for developers by creating an open-source and cross-platform tool that is both feature-rich and beginner-friendly.
           </p>
@@ -81,11 +83,11 @@ const AboutPage: Page = ({ initialLoad, reduceMotion }) => {
         <section aria-labelledby="why-another-git-client" className="flex flex-col gap-4">
           <h2
             id="why-another-git-client"
-            className="text-3xl text-center text-primary"
+            className="text-2xl text-center lg:text-3xl text-primary"
           >
             Why another Git client?
           </h2>
-          <p className="text-lg">
+          <p className="lg:text-lg">
             At Commit Rocket, we believe that the current generation of Git clients is outdated and slow.
             We want to change that by providing a sleek and fast client that meets the needs of today's developers.
             Our commitment to open-source development and cross-platform compatibility sets us apart from other clients in the market.
@@ -94,24 +96,19 @@ const AboutPage: Page = ({ initialLoad, reduceMotion }) => {
         <section aria-labelledby="why-another-git-client" className="flex flex-col gap-4 my-8">
           <h2
             id="why-another-git-client"
-            className="text-3xl text-center text-primary"
+            className="text-2xl text-center lg:text-3xl text-primary"
           >
             Team
           </h2>
           <motion.div
-            className="flex justify-center gap-4"
+            className="flex flex-wrap justify-center gap-4"
             aria-label="Members"
             initial="hidden"
             animate="show"
             variants={membersContainerAnim}
           >
             {members.map((member, i) => (
-              <motion.div
-                key={i}
-                variants={(reduceMotion || initialLoad) ? {} : memberAnim}
-              >
-                <Member {...member} />
-              </motion.div>
+              <Member key={i} animate={!(reduceMotion || initialLoad)}{...member} />
             ))}
           </motion.div>
         </section>
