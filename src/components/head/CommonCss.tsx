d050b80b73024a1b0cccf82af840263515f2785c
RikThePixel <Rikdenbreejen@hotmail.nl>
2023-04-02 13:53:30 +0200

    Compress common css

--- src/components/head/CommonCss.tsx
+++ src/components/head/CommonCss.tsx
@@ -1,19 +1,35 @@
 import React from "react";
 
 const CommonCss = () => {
-  return (
-    <style>
-      {`
-        :where(.w-full) { width: 100%; }
-        :where(.flex) { display: flex; }
-        :where(.items-center) { align-items: center; }
-        :where(.justify-center) { justifiy-content: center }
+  const css = `
+    :where(.w-full) {
+       width: 100%; 
+    }
 
-        
-        details>summary { list-style: none; }
-        details>summary::-webkit-details-marker { display: none; }
-      `}
-    </style>
+    :where(.flex) { 
+      display: flex; 
+    }
+
+    :where(.items-center) { 
+      align-items: center; 
+    }
+
+    :where(.justify-center) { 
+      justifiy-content: center 
+    }
+
+    details > summary { 
+      list-style: none;
+     }
+    details > summary::-webkit-details-marker { 
+      display: none; 
+    }
+
+    `.replace(/\s*\n*\t*/gm, "");
+  return (
+    <style dangerouslySetInnerHTML={{
+      __html: css
+    }} />
   );
 };
 
